30e2b301d40166b4a15929c23c4bd418ab8f740c
dogerpve <dogerpve@ukr.net>
2018-05-31 18:43:26 +0300

    my_first_commit

---
+++ lesson_for_test.py
@@ -0,0 +1,14 @@
+num = 1
+secret_pin_code = 9999
+
+while True:
+    print('AAAAAAAAA')
+    # user_pin_code = int(input('Enter your pin code'))
+    # print('you userd {} attempt out of 5'.format(num))
+    # if user_pin_code == secret_pin_code:
+    #     print('Accessed password!!!')
+    #     num = 6
+    # else:
+    #     num += 1
+    #     if num <= 5:
+    #         print('Try again')
